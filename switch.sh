ba24ba0e1b3fd8b3e09b2baa691e83a6782d0ed3
Shwgupta@12 <guptashweta23797@gmail.com>
2022-05-27 00:31:11 +0845

    switch files

---
+++ switch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+empRatePerHr=20;
+isPartTime=1;
+isFullTime=2;
+workingDays=30;
+
+for (( day=1; day<=$workingDays; day++ ))
+do
+
+empcheck=$((RANDOM%3));
+
+case $empcheck in
+$isFullTime)
+                empHrs=8;;
+$isPartTime)
+                empHrs=4;;
+*)
+                empHrs=0;;
+
+esac
+
+salary=$(( $empHrs * $empRatePerHr ));
+
+done
+
+echo "Employee has earned $salary in a month"
+
+
